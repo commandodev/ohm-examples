05f4f0352483e59232920b3adf129a8c993587d3
Ben Ford <ben@fynder.io>
2015-03-02 20:53:15 +0000

    Use cabal-install from ghc7101

--- todo-mvc/shell.nix
+++ todo-mvc/shell.nix
@@ -1,10 +1,11 @@
 with (import <nixpkgs> {}).pkgs;
+with (import <nixpkgs/pkgs/development/haskell-modules/lib.nix> {pkgs = (import <nixpkgs> {});});
 let
     hsPackages = haskell-ng.packages.ghcjs.override {
       overrides = self: super: {
         virtual-dom = self.callPackage ./virtual-dom {};
         oHm = self.callPackage ./oHm {};
-        todo-mvc = self.callPackage ./. {};
+        todo-mvc = self.callPackage ./. { cabal-install = haskell-ng.packages.ghc7101.cabal-install; };
       };
     };
 in
